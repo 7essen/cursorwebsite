4d77233a5a82c5a9bb8d9c18d8612410a86f6f4d
7essen <hussona4635@gmail.com>
2024-12-11 18:12:03 +0200

    HI

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
@@ -71,4 +71,4 @@ process.on('SIGINT', () => {
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-}); 
+});
